ade88487a09b9796b08d46274149b3ee8f6f9634
Roxx1ne <liontinperak25@mail.com>
2023-11-28 22:53:26 +0700

    splash screen

---
+++ android/app/src/main/kotlin/com/example/projek_ambata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projek_ambata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'home.dart';
+import 'main.dart';
+
+class splash extends StatefulWidget {
+  const splash({super.key});
+
+  @override
+  State<splash> createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigtatetohome();
+  }
+
+  _navigtatetohome() async {
+    await Future.delayed(Duration(milliseconds: 10000), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: "next halaman login",
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Latar Belakang
+          Image.asset(
+            'assets/img/cover.png',
+            fit: BoxFit.cover,
+          ),
+
+          Positioned(
+            bottom: 30, // Sesuaikan dengan posisi yang diinginkan dari bawah
+            left: (MediaQuery.of(context).size.width - 100) / 2 -
+                5, // Sesuaikan nilai ini
+            child: Lottie.asset(
+              'assets/img/loading2.json',
+              width: 100,
+              height: 100,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
